35bfd22a934cb269f843c738ef15ef7a40c20a8f
Zaira <133662712+ZairaSSilveira@users.noreply.github.com>
2024-05-27 09:10:02 -0300

    Adicionar arquivos de projeto.

---
+++ lt4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace lt4 {
+
+    class Projeto
+{
+
+    static void Main(String[] args)
+    {
+
+        /*int num = 5;
+        for (int i = 1; i <= 10; i++)
+        {
+            int resultado = num * i;
+            Console.WriteLine("");
+            Console.WriteLine(num + "x" + i + " = " + resultado);
+        }*/
+
+
+
+         /*Console.WriteLine("Digite um número:");
+         int numero = int.Parse(Console.ReadLine());
+
+         for (int i = 0; i <= numero; i += 3)
+         {
+             Console.WriteLine(i + " ");
+         }*/
+
+
+
+       /* for (int i = 0; i <5; i++) {
+
+            Console.WriteLine("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero > 100)
+            {
+                Console.WriteLine("NÚMERO ESPECIAL");
+            }
+            else
+            {
+                Console.WriteLine("NÚMERO COMUM");
+            }
+        }*/
+
+
+       /* Console.WriteLine("Deseja continuar?");
+        char resposta = char.Parse(Console.ReadLine());
+        while (resposta == 's')
+        {
+            Console.WriteLine("Deseja continuar?");
+            resposta = char.Parse(Console.ReadLine()) ;
+        }*/
+
+    }
+}
+}
